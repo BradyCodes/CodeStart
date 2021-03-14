139d3d6ddc80a2d71be92fdf129007a303a49d0b
Brady Redding <44198185+BradyCodes@users.noreply.github.com>
2021-03-14 18:28:00 -0400

    Initial Upload 0.0.1

---
+++ app.js
@@ -0,0 +1,16 @@
+function redir(page){
+    window.location.href = page;
+}
+
+
+var dropdown = document.querySelector("nav .dropdown");
+var button = document.querySelector("nav .menu");
+dropdown.style.display = "none";
+function drop(){
+    if (dropdown.style.display === "none"){
+        dropdown.style.display = "grid";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
+
