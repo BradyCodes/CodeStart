7f8d59feb80729aa0ae75d9f9f20ef0e01b25f0c
Brady Redding <44198185+BradyCodes@users.noreply.github.com>
2021-03-24 17:42:56 -0400

    Added Redir functionality

--- emailjsfunctionality.js
+++ emailjsfunctionality.js
@@ -18,9 +18,13 @@
         emailjs.send('service_8y5p22k', 'template_g86a9wo', templateParams, 'user_9GIoGw8r0z8WrEqHbdUkV')
             .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
+            setTimeout(() => {
+                document.location.href = "redir.html";
+            }, 1000);
             }, function(error) {
             console.log('FAILED...', error);
+            document.write('Something went wrong... Refresh the page and try again - sorry for the inconvenience!')
         });
        };
-       rsend();  
+       rsend();
     };
