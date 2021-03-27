27e79dd74fb2a97abe89955115f8bf781888cd2d
Brady Redding <44198185+BradyCodes@users.noreply.github.com>
2021-03-26 23:06:40 -0400

    Dual Email Functions added

--- app.js
+++ app.js
@@ -13,20 +13,3 @@ function drop(){
         dropdown.style.display = "none";
     }
 }
-
-
-
-function popup(main, sub){
-    document.getElementById('myModal').style.display = 'grid';
-    var h1 = document.getElementById('maintext');
-    var h3 = document.getElementById('subtext');
-
-    h1.innerHTML = main;
-    h3.innerHTML = sub;
-}
-
-
-document.getElementById('close').addEventListener('click', function(){
-    document.getElementById('myModal').style.display = 'none';
-});
-
--- emailjsfunctionality.js
+++ emailjsfunctionality.js
@@ -15,16 +15,24 @@
         };
         
         function rsend(){
+        //sends to person registering
+        emailjs.send('service_8y5p22k', 'template_hbtj6yf', templateParams, 'user_9GIoGw8r0z8WrEqHbdUkV')
+            .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+            console.log('FAILED...', error);
+        });
+       };
+       //sends to me
         emailjs.send('service_8y5p22k', 'template_g86a9wo', templateParams, 'user_9GIoGw8r0z8WrEqHbdUkV')
             .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
-            setTimeout(() => {
-                document.location.href = "redir.html";
-            }, 1000);
+            //setTimeout(() => {
+            //    document.location.href = "redir.html";
+            //}, 1000);
             }, function(error) {
             console.log('FAILED...', error);
             document.write('Something went wrong... Refresh the page and try again - sorry for the inconvenience!')
         });
-       };
-       rsend();
+        rsend();
     };
