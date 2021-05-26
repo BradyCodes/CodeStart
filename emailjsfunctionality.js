6c6c41e87038869fab2917e901b6a278322904b0
Brady Redding <jbredding24@gmail.com>
2021-05-26 11:18:18 -0400

    Finished :)))))

--- emailjsfunctionality.js
+++ emailjsfunctionality.js
@@ -1,5 +1,5 @@
 (function() {
-    emailjs.init("");
+    emailjs.init("user_9GIoGw8r0z8WrEqHbdUkV");
 })();
 
 
@@ -16,7 +16,7 @@
         
         function rsend(){
         //sends to person registering
-        emailjs.send('', '', templateParams, '')
+        emailjs.send('service_8y5p22k', 'template_hbtj6yf', templateParams, 'user_9GIoGw8r0z8WrEqHbdUkV')
             .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
             }, function(error) {
@@ -24,11 +24,11 @@
         });
        };
        //sends to me
-        emailjs.send('', '', templateParams, '')
+        emailjs.send('service_8y5p22k', 'template_g86a9wo', templateParams, 'user_9GIoGw8r0z8WrEqHbdUkV')
             .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
             setTimeout(() => {
-                document.location.href = "redir.html";
+                document.location.href = "../redir.html";
             }, 1000);
             }, function(error) {
             console.log('FAILED...', error);
